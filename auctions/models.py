abcd6a1c15954de27a9f7b5a6d976a7464d55929
ni1985 <kir.nikel@gmail.com>
2021-03-12 22:27:19 +0100

    Models are created

--- auctions/models.py
+++ auctions/models.py
@@ -6,24 +6,48 @@ class User(AbstractUser):
     pass
 
 class Listing(models.Model):
-    title
-    owner
-    description
-    start_bid
-    url
-    category
-    finished
-    winner
+    
+    CATEGORY_CHOICES = [
+        ('bk', 'Books'),
+        ('no', 'No category listed'),
+        ('ki', 'Kitchen'),
+        ('gd', 'Garden'),
+        ('mg', 'Magic'),
+    ]
+    
+    title = models.CharField(max_length=30)
+    owner = models.ForeignKey(
+        User, 
+        on_delete = models.CASCADE,
+        related_name='owner')
+    description = models.TextField()
+    start_bid = models.IntegerField()
+    url = models.URLField()
+    category = models.CharField(choices=CATEGORY_CHOICES, default = "no", max_length=2)
+    finished = models.BooleanField()
+    winner = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE,
+        related_name='winner')
 
 class Bid(models.Model):
-    user_id
-    listing_id
-    time
-    bid
+    user_id = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE)
+    listing_id = models.ForeignKey(
+        Listing,
+        on_delete = models.CASCADE)
+    date_time = models.DateTimeField(auto_now_add=True)
+    bid = models.IntegerField()
+    winner = models.BooleanField()
 
 class Comments(models.Model):
-    user_id
-    listing_id
-    com
-    time
+    user_id = models.ForeignKey(
+        User,
+        on_delete = models.CASCADE)
+    listing_id = models.ForeignKey(
+        Listing,
+        on_delete = models.CASCADE)
+    com = models.TextField()
+    date_time = models.DateTimeField(auto_now_add=True)
 
